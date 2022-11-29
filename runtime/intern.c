9119d20536bf0ca9d25afc0c3c5459eb204c7b0b
root <root@DESKTOP-0S8CSQT.localdomain>
2022-11-29 15:27:51 +0000

    revert Duplicate values for Forward_tag

--- runtime/intern.c
+++ runtime/intern.c
@@ -370,8 +370,6 @@ static void intern_rec(value *dest)
       if (size == 0) {
         v = Atom(tag);
       } else {
-        if (tag == 250 || tag == 248)
-          tag = Forward_tag; /* Current Forward_tag value */
         v = Val_hp(intern_dest);
         if (intern_obj_table != NULL) intern_obj_table[obj_counter++] = v;
         *intern_dest = Make_header(size, tag, intern_color);
