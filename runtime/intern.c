0490a7bff7f0916a92a5ef1a737e5eb8ea3295c9
Sara Mahmmud <saragrace4@gmail.com>
2023-03-04 01:00:18 +0000

    Closure_tag and Object_tag

--- runtime/intern.c
+++ runtime/intern.c
@@ -370,6 +370,10 @@ static void intern_rec(value *dest)
       if (size == 0) {
         v = Atom(tag);
       } else {
+        if (tag == 245 || tag == 248)
+          tag = Object_tag;
+        else if (tag == 244 || tag == 247)
+          tag = Closure_tag;
         v = Val_hp(intern_dest);
         if (intern_obj_table != NULL) intern_obj_table[obj_counter++] = v;
         *intern_dest = Make_header(size, tag, intern_color);
