b01454448fd3a2a55c4a84bc9c5e65a342748655
saramahmmud <saragrace4@gmail.com>
2023-05-01 12:47:26 +0000

    str_comp_2

---
+++ str_comp2.ml
@@ -0,0 +1,30 @@
+let physically_equal = ref 0 in
+
+let rec pairwise_compare lst =
+  match lst with
+  | [] -> []
+  | x :: xs ->
+    let cmp x y = 
+      (assert ((Obj.tag (Obj.repr x)) = 252);
+      (if (x == y) then physically_equal := !physically_equal + 1); 
+      assert((String.compare x y)=0);) 
+    in
+    let pairs = List.map (fun y -> (x, y)) xs in
+    List.iter (fun (a, b) -> cmp a b) pairs;
+    pairwise_compare xs
+in
+
+let t  = String.make 200 'a' in
+let x = ref [t] in
+
+for i = 0 to 10000 - 2 do
+  let y = String.make 200 'a' in
+  x :=  y :: !x;   
+done;
+
+Gc.minor();
+let _ = pairwise_compare !x in
+let _ = Printf.printf "physical: %i\n" (!physically_equal) in
+
+Gc.full_major();
+Gc.print_stat(stdout)
